549d582e94f259f9d2b95bd9f7f88b6d0bedb836
John Goodman <johngood@cisco.com>
2018-12-17 14:49:20 -0500

    example background with parallax

--- src/entities/space_background.js
+++ src/entities/space_background.js
@@ -1,4 +1,20 @@
-zombufo.e('Space Setting', function() {
-  return Crafty.e('Space, Color')
-    .background('zombufo/src/assets/space_background.jpg');
+zombufo.e('Space Setting', function(viewport) {
+  var darkness = Crafty.e('Space, DOM, 2D, Color')
+    .attr({w: 600, h: 600, x: 0, y: 0, z: -5})
+    .color("#001133")
+
+  var stars1 = Crafty.e('Space, DOM, 2D, Image')
+    .attr({w: 600, h: 600, x: 0, y:0, z: -4})
+    .image('src/assets/space_background_stars_1.png', "repeat");
+  
+  var stars2 = Crafty.e('Space, DOM, 2D, Image')
+    .attr({w: 600, h: 600, x: 0, y: 0, z: -3})
+    .image('src/assets/space_background_stars_2.png', "repeat");
+  
+  Crafty.bind('InvalidateViewport', function() {
+    stars1.x = viewport.x / 10;
+    stars1.y = viewport.y / 10;
+    stars2.x = viewport.x / 5;
+    stars2.y = viewport.y / 5;
+  });
 });
--- src/scenes/level_1.js
+++ src/scenes/level_1.js
@@ -1,11 +1,14 @@
 Crafty.defineScene('Level 1', function(setup) {
   console.log('Level 1!');
+  zombufo.e('Space Setting', Crafty.viewport);
   zombufo.e('Keyboard');
   zombufo.e('GreenSquare')
     .attr({x: 50, y:50})
     .size(30)
     .origin('center');
-  zombufo.e('RedSquare')
-    .attr({x: 30, y:10})
-    .size(24);
+  Crafty.viewport.follow(
+    zombufo.e('RedSquare')
+      .attr({x: 30, y:10})
+      .size(24)
+  );
 });
